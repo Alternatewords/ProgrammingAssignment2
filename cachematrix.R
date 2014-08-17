828e9c35bd11c13b06ed77d58653f9f2ab6d7697
Alternatewords <thorstenbecker@gmx.de>
2014-08-17 12:46:01 +0200

    Increased indention for readability

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,17 @@
 ## Example run: a<-makeCacheMatrix(), a$set(matrix(1:4,2,2)
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-       x <<- y
-       m <<- NULL
-  }
-  get <- function() x
-  setmatrix <- function(solve) m <<- solve
-  getmatrix <- function() m
-  list(set = set, get = get,
-       setmatrix = setmatrix,
-       getmatrix = getmatrix)
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
@@ -25,13 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Example second run: cacheSolve(a) > cache !is.null > retrieve cache, display message
 
 cacheSolve <- function(x = matrix(), ...) {
-  m <- x$getmatrix()
-  if (!is.null(m)) {
-       message("getting cached data")
-       return(m)
-  }
-  matrix <- x$get()
-  m <- solve (matrix, ...)
-  x$setmatrix(m)
-  m
+        m <- x$getmatrix()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve (matrix, ...)
+        x$setmatrix(m)
+        m
 }
