2db67a095c2e1a1ab051027c1e2ea715723e2b80
Alternatewords <thorstenbecker@gmx.de>
2014-08-17 14:04:39 +0200

    Updates/corrected comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 ## This is simply a re-write of the example functions provided for the Coursera MOOC "R Programming"
 ## programming assignment 2. 
 
-## Function creates a special matrix object that caches its inverse.
-## Example run: a<-makeCacheMatrix(), a$set(matrix(1:4,2,2)
+## Function makeCacheMatrix creates a special matrix object that caches its inverse.
+## Example run: a <- makeCacheMatrix(), a$set(matrix(1:4,2,2))
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -19,9 +19,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## First run: Check if cached data exists > does-not > cache data
-## Second run: Check if cached data exists > does > pull cache, display confirmation message
-## Example first run: cacheSolve(a) > cache is empty > cache matrix
+## Function cacheSolve computes the inverse of the special "matrix" object returned by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed), then cachesolve retrieves
+## the inverse from the cache.
+## Example first run: cacheSolve(a) > cache is.null > cache inverse matrix
 ## Example second run: cacheSolve(a) > cache !is.null > retrieve cache, display message
 
 cacheSolve <- function(x = matrix(), ...) {
